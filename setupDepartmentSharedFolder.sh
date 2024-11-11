6c065af1cc5acc8d97916ca07d99e785cf468d33
Mason P <mason.peckham03@gmail.com>
2024-11-11 16:32:33 -0600

    My intitial commit. First creation.

---
+++ setupDepartmentSharedFolder.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+groups=('Sales' 'HumanResources' 'TechnicalOperations' 'Helpdesk' 'Research')
+
+
+BASE=$1
+
+if  [ -z "$BASE" ]; then
+ echo
+ echo "Creates a department shared folder structure at the specified destination"
+ echo
+ echo "  Usage $0 <root folder> "
+ echo
+
+ exit 1
+fi
+
+USER=`whoami`
+
+if [ $USER != "root" ]; then
+ echo "Root permission is required."
+ exit 1
+fi
+
+for group in "${groups[@]}"; do
+ if [ ! $(getent group $group) ]; then
+  echo "Creating group $group."
+  groupadd $group
+ fi
+
+ folder="$BASE/$group"
+ if [ ! -d "$folder" ]; then
+  echo "Creating shared folder at $folder."
+  mkdir -p "$folder"
+ fi
+
+ echo " - Applying $USER:$group ownership on $folder..."
+ chown "$User:$group" "$folder"
+
+ echo " - Applying permissions on $folder... $USER=rwx,$group=rwx,o=---"
+ chmod u+rwx,g+rwx,o-rwx "$folder"
+
+ echo " - Granting permission (rx) to Helpdesk on $folder..."
+ setfacl --modify=g:Helpdesk:rx "$folder"
+done
